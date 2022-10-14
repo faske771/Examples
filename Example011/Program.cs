f37e586b9b8930b06829abf3b7b1455751840c85
Artr <example.com>
2022-10-14 18:18:35 +0300

    ex11

---
+++ Example011/Program.cs
@@ -0,0 +1,42 @@
+﻿void FillArray(int[] collection)
+{
+    int Length = collection.Length;
+    int index = 0;
+    while (index<Length)
+    {
+        collection[index] = new Random().Next(1,10);
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int possition = 0;
+    while (possition < count)
+    {
+        Console.WriteLine(col[possition]);
+        possition++;
+    }
+}
+int[] array = new int[10];
+int indexof(int[] collection, int find)
+{
+  int count = collection.Length;
+  int index = 0;
+  int possition = -1;
+  while (index<count)
+  {
+    if (collection[index] == find)
+    {
+        possition = index;
+        break;
+    }
+    index++;
+  }
+  return possition;  
+}
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int pos = indexof(array, 4);
+Console.WriteLine(pos);
