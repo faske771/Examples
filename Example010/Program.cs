83e2905e798f8785ab899e7f71d88f5fc131e3a2
Artr <example.com>
2022-10-14 18:00:40 +0300

    ex10

---
+++ Example010/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = {1, 12, 13, 14, 1345, 42423, 3131, 41, 42423, 3123};
+int n = array.Length;
+int find = 42423;
+int index = 0;
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index); 
+        break;
+    }
+    index = index + 1;
+}
